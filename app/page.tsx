a838069053af25d892b9ae4b3a161e7f77eb715e
yamagame <yamagame@mac.com>
2023-07-01 11:06:41 +0900

    メッセージ対応

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,21 @@ import React from "react"
 import { Canvas } from "@react-three/fiber"
 import { Box } from "./box"
 
+type BoxT = { x: number; y: number; angle: number }
+
 export default function Home() {
   const socketRef = React.useRef<WebSocket>()
+  const [boxes, setBoxes] = React.useState<BoxT[]>([])
   React.useEffect(() => {
     socketRef.current = new WebSocket("ws://localhost:9001/socket")
     socketRef.current.onmessage = function (msg) {
       const payload = JSON.parse(msg.data)
       const { id, x, y, angle } = payload
+      setBoxes((boxes) => {
+        const b = [...boxes]
+        b[id] = { x, y, angle }
+        return b
+      })
       console.log(payload, id, x, y, angle)
     }
     socketRef.current.onopen = function () {
@@ -31,10 +39,9 @@ export default function Home() {
       <Canvas camera={{ fov: 35, near: 0.1, far: 1000, position: [10, 10, 10] }}>
         <ambientLight />
         <pointLight position={[0, 10, 10]} />
-        <Box position={[-1, 0, -1]} />
-        <Box position={[-1, 0, 1]} />
-        <Box position={[1, 0, -1]} />
-        <Box position={[1, 0, 1]} />
+        {boxes.map((box: BoxT, id: number) => {
+          return <Box key={id} position={[box.x, 0, box.y]} />
+        })}
       </Canvas>
     </div>
   )
