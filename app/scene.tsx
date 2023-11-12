1e772fd40dd6102c275252e5441cfb89bbcb9ec5
yamagame <yamagame@mac.com>
2023-11-12 20:42:06 +0900

    カメラコントロールの基本実装

--- app/scene.tsx
+++ app/scene.tsx
@@ -11,14 +11,21 @@ type BoxT = { x: number; y: number; angle: number }
 
 type GLTFResult = GLTF & {
   nodes: {
-    building: THREE.Mesh
+    cuboid1: THREE.Mesh
+    cuboid2: THREE.Mesh
+    cuboid3: THREE.Mesh
+    cuboid4: THREE.Mesh
+    ["camera-point"]: THREE.Mesh
   }
   materials: {}
 }
 
+type NAMES = "cuboid1" | "cuboid2" | "cuboid3" | "cuboid4" | "camera-point"
+
 export function Scene(props: { boxes: { [index: string]: BoxT } }) {
-  const gltfModel = useGLTF("/cube.glb") as GLTF
-  const { nodes } = useGLTF("/cube-transformed.glb") as GLTFResult
+  // const gltfModel = useGLTF("/cube.glb") as GLTF
+  const { nodes } = useGLTF("/cuboids-transformed.glb") as GLTFResult
+  const [pointCamera, setPointCamera] = useState("")
   // const gltfModel = useGLTF("/sample-building.glb") as GLTF
 
   const ref = useRef<THREE.Mesh>(null)
@@ -40,62 +47,85 @@ export function Scene(props: { boxes: { [index: string]: BoxT } }) {
       ref.current.material.color.lerp(color.set("#fa2720"), 0.1)
     }
   })
+
+  const cameraDirection = () => {
+    const cameraTarget = new THREE.Vector3()
+    cameraControlsRef.current?.getTarget(cameraTarget)
+    const cameraPosition = new THREE.Vector3()
+    cameraControlsRef.current?.getPosition(cameraPosition)
+    return cameraTarget.sub(cameraPosition)
+  }
+
   return (
     <>
       <Sky />
       <ambientLight intensity={0.1} />
       <pointLight position={[-2, 10, 10]} />
       <directionalLight position={[2, 5, -5]} intensity={0.5} />
-      {/* {Object.keys(props.boxes).map((key: string) => {
-        const box = props.boxes[key]
-        return <Box key={key} position={[box.x, 0, box.y]} rotation={[0, box.angle, 0]} />
-      })} */}
-      <CameraControls
-        ref={cameraControlsRef}
-        // minDistance={3}
-        enabled={true}
-        // verticalDragToForward={verticalDragToForward}
-        // dollyToCursor={dollyToCursor}
-        // infinityDolly={infinityDolly}
-      />
-      <Box
-        size={[2, 5, 3]}
-        position={[4, 0, 0]}
-        rotation={[0, 0, 0]}
-        onClick={() => {
-          cameraControlsRef.current?.moveTo(4, 0, 0, true)
-          // cameraControlsRef.current?.setPosition(4, 0, 0.1, true)
-        }}
-      />
-      <Box
-        scale={0.1}
-        position={[0, 0, 0]}
-        rotation={[0, 0, 0]}
-        onClick={() => {
-          const center = nodes.building.geometry.boundingBox?.getCenter(new THREE.Vector3())
-          if (center) {
-            const values = center.multiplyScalar(0.1).toArray()
-            console.log(values)
-            cameraControlsRef.current?.moveTo(...values, true)
-          }
-        }}
-        // object={gltfModel}
-        geometry={nodes.building.geometry}
-      />
-      <Box
-        size={[1, 3, 2]}
-        position={[-3, 0, 2]}
-        rotation={[0, 0, 0]}
-        onClick={() => cameraControlsRef.current?.moveTo(-3, 1.5, 2, true)}
-      />
+      <CameraControls ref={cameraControlsRef} enabled={true} />
+      {(["cuboid1", "cuboid2", "cuboid3", "cuboid4"] as NAMES[]).map((geometory_name) => {
+        return (
+          <Box
+            key={geometory_name}
+            // scale={0.1}
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            onClick={() => {
+              const center = nodes[geometory_name].geometry.boundingBox?.getCenter(
+                new THREE.Vector3()
+              )
+              if (center) {
+                const values = center.toArray()
+                console.log(values)
+                cameraControlsRef.current?.moveTo(...values, true)
+
+                const direction = cameraDirection()
+                console.log(direction.length())
+                if (direction.length() < 3) {
+                  const direction = cameraDirection().normalize().multiplyScalar(-3)
+                  const cameraPosition = new THREE.Vector3()
+                  cameraControlsRef.current?.getPosition(cameraPosition)
+                  cameraControlsRef.current?.setPosition(
+                    ...cameraPosition.add(direction).toArray(),
+                    true
+                  )
+                }
+                setPointCamera("")
+              }
+            }}
+            geometry={nodes[geometory_name].geometry}
+          />
+        )
+      })}
+      {(["camera-point"] as NAMES[])
+        .filter((name) => pointCamera !== name)
+        .map((geometory_name) => {
+          return (
+            <Box
+              key={geometory_name}
+              // scale={0.1}
+              position={[0, 0, 0]}
+              rotation={[0, 0, 0]}
+              onClick={() => {
+                const center = nodes[geometory_name].geometry.boundingBox?.getCenter(
+                  new THREE.Vector3()
+                )
+                if (center) {
+                  const direction = cameraDirection().normalize().multiplyScalar(-0.01)
+                  const target = direction.add(center)
+                  cameraControlsRef.current?.moveTo(...center.toArray(), true)
+                  cameraControlsRef.current?.setPosition(...target.toArray(), true)
+                  setPointCamera(geometory_name)
+                }
+              }}
+              geometry={nodes[geometory_name].geometry}
+            />
+          )
+        })}
       <Text ref={ref} position={[0, 0, 2]} {...fontProps}>
         {/* 日本語のテスト */}
         Hello
       </Text>
-      {/* <mesh scale={0.02} onClick={() => cameraControlsRef.current?.moveTo(0, 0, 0, true)}>
-        <primitive object={gltfModel.scene} />
-      </mesh> */}
-      {/* <Model /> */}
     </>
   )
 }
