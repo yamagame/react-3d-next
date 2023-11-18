99f9843a9c4fab397c7843c653080650ec2145c6
yamagame <yamagame@mac.com>
2023-11-18 14:43:57 +0900

    socket 処理を削除

--- app/page.tsx
+++ app/page.tsx
@@ -36,34 +36,7 @@ function Overlay() {
 }
 
 export default function Home() {
-  const socketRef = React.useRef<WebSocket>()
-  const [boxes, setBoxes] = React.useState<{ [index: string]: BoxT }>({})
   const sceneRef = React.useRef<SceneHandler>()
-  React.useEffect(() => {
-    socketRef.current = new WebSocket("ws://localhost:9001/socket")
-    socketRef.current.onmessage = function (msg) {
-      const payload = JSON.parse(msg.data)
-      const { id, x, y, angle } = payload
-      setBoxes((boxes) => {
-        const b = { ...boxes }
-        b[id] = { x, y, angle }
-        return b
-      })
-      console.log(payload, id, x, y, angle)
-    }
-    socketRef.current.onopen = function () {
-      console.log("Connected")
-    }
-    socketRef.current.onclose = function () {
-      console.log("closed")
-    }
-    return () => {
-      if (socketRef.current == null) {
-        return
-      }
-      socketRef.current.close()
-    }
-  }, [])
   return (
     <div className="main-canvas">
       <div className="nav">
