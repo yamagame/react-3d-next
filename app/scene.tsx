8e2d45c70cf82f106fbb55e431561f70ca3da905
ryusuke-m <miyaryu.tymuec@outlook.jp>
2023-12-23 12:15:33 +0900

    fix a few bugs

--- app/scene.tsx
+++ app/scene.tsx
@@ -392,8 +392,8 @@ export const Scene = React.forwardRef((props: SceneProps, ref) => {
                 scale={5}
                 castShadow
                 receiveShadow
-                position={//currentPosition
-                          calcCurrentPosition(currentPositionCtl.latitude,currentPositionCtl.longitude)
+                position={currentPosition
+                          //calcCurrentPosition(currentPositionCtl.latitude,currentPositionCtl.longitude)
                         }//GPSを使うときは変更
                 rotation={[0, 0, 0]}
                 material={new THREE.MeshBasicMaterial({color:0xff0000})}
