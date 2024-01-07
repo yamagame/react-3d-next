071c248a2391f8672ce2eb6af03a28ae197023ec
yamagame <yamagame@mac.com>
2024-01-07 09:00:43 +0900

    微修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 
 // import { Scene, SceneHandler } from './components/Scene'
