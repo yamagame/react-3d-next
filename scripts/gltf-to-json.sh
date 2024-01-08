633d6baaa8b9f39c9684810054f0ef94a0f5eb1b
yamagame <yamagame@mac.com>
2024-01-08 18:33:13 +0900

    データ整理

--- scripts/gltf-to-json.sh
+++ scripts/gltf-to-json.sh
@@ -7,5 +7,6 @@ CWD=`pwd`
 BASEDIR=`dirname $1`
 FILENAME=$(basename -- $1)
 BASENAME="${FILENAME%.*}"
-node tools/glft2json/index.mjs $1 | tee ./work/$BASENAME.json
+node tools/gltf2json/index.mjs $1 | tee ./work/$BASENAME.json
 jq -s 'add' ./assets/json/$BASENAME.json ./work/$BASENAME.json > ./app/scenes/$BASENAME.json
+cp ./assets/glb/$BASENAME.glb ./public
