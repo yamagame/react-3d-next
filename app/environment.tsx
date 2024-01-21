bc8047ef4bf57395588ba946195da4afe4c295c3
yamagame <yamagame@mac.com>
2024-01-21 22:11:52 +0900

    不要なコードを削除

--- app/environment.tsx
+++ app/environment.tsx
@@ -1,34 +1,6 @@
-import React, { useState, useTransition } from 'react'
-import { useControls } from 'leva'
+import React from 'react'
 import { Environment } from '@react-three/drei'
 
-enum PresetType {
-  sunset,
-  dawn,
-  night,
-  warehouse,
-  forest,
-  apartment,
-  studio,
-  city,
-  park,
-  lobby,
-}
-
-const options = Object.keys(PresetType).filter((v) => {
-  return isNaN(+v)
-})
-
 export function Env() {
-  // const [preset, setPreset] = useState('white.png')
-  // const [inTransition, startTransition] = useTransition()
-  // const { blur } = useControls({
-  //   blur: { value: 0.65, min: 0, max: 1 },
-  //   preset: {
-  //     value: preset,
-  //     options,
-  //     onChange: (value) => startTransition(() => setPreset(value)),
-  //   },
-  // })
   return <Environment files={'venice_sunset_1k.hdr'} background blur={0.65} />
 }
