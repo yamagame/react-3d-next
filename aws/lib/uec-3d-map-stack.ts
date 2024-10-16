13a7cb241ae88a1b99aac05e2f0555e27907ff9e
Keita Ito <eguchi@eguchi.cc>
2024-10-16 18:30:23 +0900

    feat: s3とcloudfrontを構成

--- aws/lib/uec-3d-map-stack.ts
+++ aws/lib/uec-3d-map-stack.ts
@@ -1,16 +1,25 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins'
 
 export class Uec3DMapStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'Website', {
+      bucketName: `${this.account}-uec-3d-map`,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Uec3DMapQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: cloudfront_origins.S3BucketOrigin.withOriginAccessControl(bucket),
+      },
+    })
+
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+    })
   }
 }
